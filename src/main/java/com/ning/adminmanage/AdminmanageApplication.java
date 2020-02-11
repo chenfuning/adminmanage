bccb71bca0b3db6eb18e619de243adf3bf62583e
chengfuning <1097984277@qq.com>
2020-02-11 10:57:05 +0800

    构建项目基本架构

---
+++ src/main/java/com/ning/adminmanage/AdminmanageApplication.java
@@ -0,0 +1,13 @@
+package com.ning.adminmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ning/adminmanage/AdminmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ning.adminmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
